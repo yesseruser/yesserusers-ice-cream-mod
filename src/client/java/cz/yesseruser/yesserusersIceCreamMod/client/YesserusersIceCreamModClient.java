f7e51b2e48da30ab0fefe32b642a32269e1be281
yesseruser <yesseruseryt@gmail.com>
2026-06-20 00:29:11 +0200

    Create empty 26.1 project

---
+++ src/client/java/cz/yesseruser/yesserusersIceCreamMod/client/YesserusersIceCreamModClient.java
@@ -0,0 +1,10 @@
+package cz.yesseruser.yesserusersIceCreamMod.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class YesserusersIceCreamModClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/cz/yesseruser/yesserusersIceCreamMod/YesserusersIceCreamMod.java
@@ -0,0 +1,10 @@
+package cz.yesseruser.yesserusersIceCreamMod;
+
+import net.fabricmc.api.ModInitializer;
+
+public class YesserusersIceCreamMod implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
